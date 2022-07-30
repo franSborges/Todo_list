9463b073506d5b95541c01cda4f748fe0545edc0
borgesfran@gmail.com <team@fastcode.com.br>
2022-07-30 17:05:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import * as C from './App.styles';
+import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
+import { Item } from './types/Item'
+
+export const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'comprar pão', done: false},
+    {id: 2, name: 'comprar luva', done: true}
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+     let newList = [...list];
+     newList.push({
+        id: list.length + 1,
+        name: taskName,
+        done: false
+     });
+     setList(newList);
+  }
+
+
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        <AddArea onEnter={handleAddTask}/>
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item}/>
+        ))}
+
+      </C.Area>
+    </C.Container>
+  );
+}
+
+
